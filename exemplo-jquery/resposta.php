8ac0065f9b3590a086a123236d452ba723dbb9e8
raul <raulsenaferreira@gmail.com>
2015-10-25 11:31:58 -0200

    adicionando apresentação de jQuery (2013) da III SECCIM

---
+++ exemplo-jquery/resposta.php
@@ -0,0 +1,35 @@
+<?php if($_GET['p'] == 'curso') : ?>
+
+<h1>Ciência da Computação</h1>
+
+<p>Ciência da computação é o estudo dos algoritmos, suas aplicações e de sua implementação, na forma de software, para execução em computadores.</p>
+
+<?php endif; ?>
+
+
+
+<?php if($_GET['p'] == 'faculdade') : ?>
+
+<h1>UFRRJ</h1>
+
+<p>A Universidade Federal Rural do Rio de Janeiro (UFRRJ) é uma centenária universidade brasileira localizada no município de Seropédica, Nova Iguaçu e Três Rios no estado do Rio de Janeiro. É a universidade com o maior campus universitário da América Latina (Seropédica).</p>
+
+<?php endif; ?>
+
+
+<?php if($_GET['p'] == 'ajax') : ?>
+
+<h1>Ajax</h1>
+
+<p>Nunca foi tão fácil usar Ajax :P</p>
+
+<?php endif; ?>
+
+
+<?php if($_GET['p'] == 'aluno') : ?>
+
+<h1>Raul Sena Ferreira</h1>
+
+<p>Quer saber mais sobre mim? Procura no google, :D</p>
+
+<?php endif; ?>
